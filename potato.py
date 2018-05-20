6e73b7f6bef437398e079ff0b42cf7424597f649
xodh <kto6278@naver.com>
2018-05-20 21:48:26 +0900

    Update potato.py

--- potato.py
+++ potato.py
@@ -1,163 +1,83 @@
 import sqlite3
 import click
-import re
+import sys
 
-def create_db():
-
-    global conn, cur
-
-    conn = sqlite3.connect("potato.db")
-    cur = conn.cursor()
-
-    table_create_sql = """create table if not exists todo (
-                id integer primary key autoincrement,
-                what text not null,
-                due text not null,
-                finished integer default 0);"""
-    
-    cur.execute(table_create_sql)
-
-def exe_mk(mk):
-
-    sql = "insert into todo (what, due) values (?, ?)"
-    task = mk
-
-    cur.execute(sql, task)
-    conn.commit()
-
-def exe_rm(rm):
-
-    sql = "delete from todo where id=?"
-    task = (rm,)
-
-    cur.execute(sql, task)
-    conn.commit()
-
-def exe_mod(mod):
-
-    cur.execute("select * from todo where 1")
-    rows = cur.fetchall()
-    cur.execute("select * from todo where id=?", (mod,))
-    row = cur.fetchall()
-
-    if mod.isdigit() and row and int(mod)<=len(rows) and mod > '0':
-        print("\n(Nothing will change if you enter nothing.)")
-
-        wh = str(input("What's your new plan?: "))
-        wh = wh if wh else row[0][1] # Check if inputs are empty; nothing will change if input is empty
-        
-        du = str(input("When is the due date? : "))
-        du = du if du else row[0][2] # Check if inputs are empty; nothing will change if input is empty
-        regular = re.compile(r"(\d{4})[-](\d{2})[-](\d{2})\s(\d{2})[:](\d{2})")
-        while True:
-            while len(du) != 16:
-                du = str(input("When is the due date? : "))
-            match = regular.match(du)
-            if match == None:
-                du = str(input("When is the due date? : "))
-            else:
-                break
-        
-
-        
-        fin = str(input("Is it finished?(Y/N) : "))
-        while(fin != 'Y' and fin != 'N' and fin !=''):
-            fin = str(input('Is it finished?(Y/N) : '))
-        if fin == "Y":
-            fin = 1
-        elif fin == "N":
-            fin = 0
-        else:
-            fin = fin if fin else row[0][3] # Check if inputs are empty; nothing will change if input is empty
-        
-
-        sql = "update todo set what=?, due=?, finished=? where id=?"
-        task = (wh, du, fin, mod)
-
-        cur.execute(sql, task)
-        conn.commit()
-
-    else:
-    	print("\nInvalid number; check it again! :(")
-
-def check_print_option(p_opt):
-
-    default = "select * from todo where 1"
-
-    return{
-
-    'unfinished': "select * from todo where finished=0",
-    'finished': "select * from todo where finished=1"
-
-    }.get(p_opt, default)
-
-def print_list(p_opt):
-
-    cur.execute(check_print_option(p_opt))
-    rows = cur.fetchall()
-
-    if rows:
-
-        print(
-            "\n"
-            "                   P O T A T O F I E L D                 \n"
-            "=========================================================\n"
-            "| No.|   Description   |       Due        |   Status    |\n"
-            "========================================================="
-            )
-
-        for row in rows:
-
-            #Get the columns
-            num = row[0]
-            wh = row[1]
-            du = row[2]
-            fin = row[3]
-
-            print(
-                #Print the number of plan; Max : 2-digit number
-                "|", str(num).ljust(2),
-                #Print @#$^... if the description is too long to print; Max : 15 chars
-                "|", wh.ljust(15) if len(wh)<=15 else wh[:12] + "...",
-                #Print the due as it is since the due has its own format; YYYY-MM-DD
-                "|", du,
-                #Print whether the plan is done or not
-                "| Done        |" if fin==1 else "| In progress |"
-                )
-
-        print(
-            "=========================================================\n"
-            #Dummy line, but planning to make pages
-            "                                              Page 01/01 \n")
-
-    else:
-        print("Nothing to print :(\n")
+sys.path.insert(0, './data')
+from db import DB
+from make import Make
+from remove import Remove
+from modify import Modify
+from find import Find
+from detail import Detail
+from plan import Plan
 
 @click.command()
+
 #Basic options
-@click.option('--mk', nargs=2, type=str, help='Make a new plan: [descr.] [due]')
-@click.option('--rm', type=int, help='Remove your plan: [number]')
+@click.option('--mk', nargs=3, type=str, help='Make a new plan: [descr.] [due] [category]')
+@click.option('--rm', type=click.IntRange(1,), help='Remove your plan: [number]')
 @click.option('--mod', help='Modify your plan: [number]')
+@click.option('--find', type=str, help='Find your plan: [text]')
+@click.option('--det', type=click.IntRange(1,), help='Show details of plan: [number]')
+
 #Printing options
+@click.option('--pg', type=click.IntRange(0,), default=0, help='Print the page of which you enter: [page]')
+@click.option('--cat', 'p_opt', flag_value='category', help='Print the plans for category which you will select')
 @click.option('--uf', 'p_opt', flag_value='unfinished', help='Print your unfinished plans')
 @click.option('--f', 'p_opt', flag_value='finished', help='Print your finished plans')
-def run(mk, rm, mod, p_opt):
 
-    create_db()
-    print_option = None
+def run(mk, rm, mod, find, det, pg, p_opt):
+	db = DB()
+	cliOption = None
+	printOption = None
 
-    #Check the option
-    if mk:
-        exe_mk(mk)
-    elif rm:
-        exe_rm(rm)
-    elif mod:
-        exe_mod(mod)
-    elif p_opt:
-        print_option = p_opt
+	#Check which option is given
+	if mk:
+		cliOption = Make(db, mk)
+	elif rm:
+		cliOption = Remove(db, rm)
+	elif mod:
+		cliOption = Modify(db, mod)
+	elif find:
+		cliOption = Find(db, find)
+	elif det:
+		cliOption = Detail(db, det)
+	elif p_opt:
+		printOption = p_opt
 
-    print_list(print_option)
-    conn.close()
+	#Dummy lines
+
+	# cur.execute("select * from todo where 1")
+	# rows = cur.fetchall()
+
+	# if rows:
+	# 	for row in rows:
+	# 		iregular = re.compile(r"(\d{4})[-](\d{2})[-](\d{2})\s(\d{2})[:](\d{2})")
+	# 		iregular2 = re.compile(r"(\d{4})[-](\d{2})[-](\d{2})\s(\d{2})[:](\d{2})")
+
+	# 		idue = row[2]
+	# 		i_match = iregular.match(idue)
+
+	# 		t = datetime.datetime.now()
+	# 		now = iregular2.match(str(t))
+
+	# 		for i in range(1,6):
+	# 			if int(i_match.group(i)) < int(now.group(i)):
+	# 				sql = "delete from todo where due = ?"
+
+	# 				cur.execute(sql, (i_match.group(0)))
+	# 				conn.commit()
+	# 			elif int(i_match.group(i)) > int(now.group(i)):
+	# 				break
+
+	if cliOption != None:
+		if cliOption.check():
+			cliOption.execute()
+		db.conn.close()
+		return
+
+	Plan(db, pg, printOption).show()
+	db.conn.close()
 
 if __name__ == '__main__':
-    run()
+	run()
